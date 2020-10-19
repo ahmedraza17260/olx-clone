f28692d575d35b0dcee6313933d945248fae7cf3
ahmedraza17260 <ahmedraza17260gmail.com>
2020-10-20 03:39:33 +0500

    remove unused variables

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { useState } from "react";
 import AddItem from "./AddItem";
 import nextId from "react-id-generator";
 
